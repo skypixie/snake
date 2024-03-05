91d41cae6bf4e59e12ffbd67714598bb29fcf9d0
deltanoru <cleanest@internet.ru>
2024-03-05 20:00:18 +0300

    Create map Create PlayerPawnBase and SnakeBase classes. Set PlayerPawnBase rotation

---
+++ Source/Snake/PlayerPawnBase.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPawnBase.h"
+
+#include "Camera/CameraComponent.h"
+#include "SnakeBase.h"
+
+
+// Sets default values
+APlayerPawnBase::APlayerPawnBase()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PawnCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PawnCamera"));
+}
+
+// Called when the game starts or when spawned
+void APlayerPawnBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetActorRotation(FRotator(0, -90, 0));
+}
+
+// Called every frame
+void APlayerPawnBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPawnBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void APlayerPawnBase::CreateSnakeActor()
+{
+	SnakeActor = GetWorld()->SpawnActor<ASnakeBase>(SnakeActorClass, FTransform());
+}
+
